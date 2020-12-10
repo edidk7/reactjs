1c397dca093617dfbcdeb1c104d75748df7202e9
edidk7 <d.ignacio292@gmail.com>
2020-12-10 19:08:26 -0300

    Componente de listado añadido (Closes #2)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
-  // Arreglo de citas
 
+  // Citas en local storage
+  let citasIniciales = JSON.parse( localStorage.getItem('citas'));
+  if(!citasIniciales){
+    citasIniciales = [];
+  }
+  
+  // Arreglo de citas
   const [citas, guardarCitas] = useState([]);
+
+  // Use Effect para realizar ciertas operaciones cuando el state cambia
+  useEffect( () => {
+    let citasIniciales = JSON.parse( localStorage.getItem('citas'));
+
+    if(citasIniciales){
+      localStorage.setItem('citas', JSON.stringify(citas))
+    }else{
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  });
 
   // Function que tome las citas actuales y agregue la nueva
   const crearCita = (cita) => {
-    guardarCitas([
-      ...citas, 
-      cita
-    ]);
+    console.log(cita);
+    guardarCitas([...citas, cita]);
   };
+
+  // Funcion que elimina una cita por su id
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  };
+
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus citas";
+
+  // console.log(citas.length);
 
   return (
     <Fragment>
@@ -21,7 +48,12 @@ function App() {
           <div className="one-half column">
             <Formulario crearCita={crearCita} />
           </div>
-          <div className="one-half column">2</div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
+          </div>
         </div>
       </div>
     </Fragment>
